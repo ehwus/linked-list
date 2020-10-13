dbb50b927f8343651a62d3fa3e01a4e665e9b600
Alex Withington-Smith <alex@awithsmith.com>
2020-10-13 06:06:45 +0100

    test to see whether value is properly returned

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,4 +5,9 @@ describe Node do
     test = Node.new("Value")
     expect(test).to be_a_kind_of(Node)
   end
+
+  it "returns its value" do
+    test = Node.new("Value")
+    expect(test.value).to eq("Value")
+  end
 end
